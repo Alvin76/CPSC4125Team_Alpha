969f5436620dc52b7c02c5b5da0e88ad274bfcf6
ericsson-james <jamesericsson95@gmail.com>
2019-09-22 16:39:45 -0400

    . test

--- random.php
+++ random.php
@@ -6,17 +6,24 @@
  <?php echo '<p>Hello World</p>'; ?>
  <a href="/index.php">Visit the home pageee.</a>
   <p>fucking hell i hate php !</p>
- </body>
- <?php require '/functions.php';
-  echo $websiteURL;
- ?>
- <?php require 'functions.php';
- echo $websiteURL;
- ?>
 
- <?php include 'functions.php';
- echo "I have a $websiteURL .";
- ?>
+  <?php include 'menu.php';
+  echo "I have a $color $car.";
+  ?>
+
+  <?php require '/functions.php';
+   echo $websiteURL;
+  ?>
+  <?php require 'functions.php';
+  echo $websiteURL;
+  ?>
+
+  <?php include 'functions.php';
+  echo "I have a $websiteURL .";
+  ?>
+
+
+ </body>
 
  <!-- <p>fucking hell i hate php !</p> -->
 </html>
