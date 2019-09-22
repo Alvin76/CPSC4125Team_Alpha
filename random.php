0de827a7e78a63adc9ac189b4830f5ef6b8d9456
ericsson-james <jamesericsson95@gmail.com>
2019-09-22 17:22:36 -0400

    . kjj

--- random.php
+++ random.php
@@ -24,6 +24,13 @@
   echo "neet";
   ?>
 
+
+  <?php
+  echo "<h2>PHP is Fun!</h2>";
+  echo "Hello world!<br>";
+  echo "I'm about to learn PHP!<br>";
+  echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+  ?>
 <p> bruh </p>
   </br>
 
