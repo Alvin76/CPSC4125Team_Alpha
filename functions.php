37df5f61e2c5598b515a481f0881f545036642a7
ericsson-james <jamesericsson95@gmail.com>
2019-09-22 19:29:46 -0400

    . fixing footer and nav bar is complete don't touch it.

--- functions.php
+++ functions.php
@@ -1,14 +1,7 @@
-<!--
-  $websiteURL = "https://test-app-ericsson.herokuapp.com/";
-
-  function footer(){
-    echo "<footer>";
-      echo "This is a footer</br>";
-    echo "</footer>";
-  } -->
 
 <?php
 
+// Defining global Variables
 $color='red';
 $car='BMW';
 $websiteURL = 'https://test-app-ericsson.herokuapp.com/';
@@ -16,58 +9,24 @@ $authors = array('James', 'Adam', 'Taylor', 'Evan');
 $nav_items = array('Home Page', 'Todays Comic', 'Random Comic');
 $urls = array('Home.php', 'todays.php', 'random.php');
 
-function config($key = '')
-{
-    $config = [
-        'name' => 'Git Gud',
-        'XKCDURL' => 'http://xkcd.com/info.0.json',
-        'nav_bar' => [
-          '' => 'Home',
-          'Home'      => "Home Page",
-          'random' => 'Random Comic',
-          'todays' => 'Todays Comic'
-        ],
-        'template_path' => 'template',
-        'content_path' => 'content',
-        'version' => 'v3.0',
-    ];
-    return isset($config[$key]) ? $config[$key] : null;
-}
 
 
-
+// Sample function for interacting with.
+// Notice the keyword global must be used before the variables can be accessed.
 function vehicle()
 {
   global $color, $car;
-  echo "I have two $color $car.";
+  echo "I have a $color $car.";
 }
 
-//function nav_bar2()
-//{
-  //$nav_bar_menu = '';
-//  $nav_items = config('nav_bar');
-  //foreach ($nav_items as $uri => $name) {
-    //$nav_bar_menu .= '<li class="nav-item"><a class="nav-link" href= "' . $name . '">'. $name .'</a></li>';
-    //echo '<li class="nav-item"><a class="nav-link" href="#">'+$name+'</a></li>';
-
-//  }
-//  echo trim($nav_bar_menu);
-//}
-
-// function nav_bar2()
-// {
-//   global $nav_items;
-//   $nav_bar_menu = '';
-//   foreach ($nav_items as $key => $name) {
-//     $nav_bar_menu .= '<li class="nav-item"><a class="nav-link" href= /"' . $name . '.php">'. $name .'</a></li>';
-//   }
-//   echo $nav_bar_menu;
-// }
-
+// This is the navbar function. If you look inside instead of using the foreach
+// I do a for loop. This allows me to index my declared arrays by a posistion.
+// I find this to be the more intutive method.
+// If you look on the appending of $nav_bar_menu you can see the html
+// that will be dynamically created on the site.
 function nav_bar()
 {
-  global $nav_items;
-  global $urls;
+  global $nav_items, $urls;
 
   for ($x = 0; $x < sizeof($nav_items) ; $x++) {
     $nav_bar_menu .= '<li class="nav-item"><a class="nav-link" href= /' . $urls[$x] . '>'. $nav_items[$x] .'</a></li>';
@@ -78,8 +37,9 @@ function nav_bar()
 function footer()
 {
   global $websiteURL;
-  echo "$websiteURL By Team Alpha! </br>";
-  echo "Team Alpha: ";
+  echo '<p> ' . $websiteURL . ' </p>';
+  // echo "$websiteURL By Team Alpha! </br>";
+  // echo "Team Alpha: ";
 }
 
 ?>
