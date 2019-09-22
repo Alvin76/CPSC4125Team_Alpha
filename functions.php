21ac9716b8e15c70144f8c4eede52878c2af5b17
ericsson-james <jamesericsson95@gmail.com>
2019-09-22 19:22:29 -0400

    . php ....

--- functions.php
+++ functions.php
@@ -70,8 +70,7 @@ function nav_bar()
   global $urls;
 
   for ($x = 0; $x < sizeof($nav_items) ; $x++) {
-  //  $nav_bar_menu .= '<li class="nav-item"><a class="nav-link" href= /"' . $url[$x] . '">'. $name[$x] .'</a></li>';
-    $nav_bar_menu .=  $nav_items[$x];
+    $nav_bar_menu .= '<li class="nav-item"><a class="nav-link" href= /' . $urls[$x] . '>'. $nav_items[$x] .'</a></li>';
   }
   echo $nav_bar_menu;
 }
