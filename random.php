d5e3397580a10168d89c974012944882b0ef7d86
ericsson-james <jamesericsson95@gmail.com>
2019-09-22 17:46:37 -0400

    . starting to come together.

--- random.php
+++ random.php
@@ -1,52 +1,32 @@
-<html>
-<?php require 'functions.php';?>
-
- <head>
-  <title>PHP Test page 2</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?>
- <a href="/index.php">Visit the home pageee.</a>
-  <p>fucking hell i hate php !</p>
+<!doctype html>
+<html lang="en">
 
 
-
-  <?php
-  echo "I have a $color $car.";
-  echo "</br> cool</br>";
-  vehicle();
-  echo "</br> Do i still have two vehicles?";
-  ?>
-
-  <p> but why ? </p>
-
-
-  <?php
-//  echo $websiteURL;
-  echo "neet";
-  ?>
-
-
-  <?php
-  echo "<h2>PHP is Fun!</h2>";
-  echo "Hello world!<br>";
-  echo "I'm about to learn PHP!<br>";
-  echo "This ", "string ", "was ", "made ", "with multiple parameters.";
-  ?>
-
-  <?php
-  echo "I have a $color $car.";
-  echo "</br> cool</br>";
-  footer();
-  echo "</br> Do i still have two vehicles?";
-  ?>
-
-<p> still working ? </p>
-  </br>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!-- Required PHP Functions -->
+    <?php require 'functions.php';?>
+    <title>Team Alpha</title>
+  </head>
 
 
 
- </body>
 
- <!-- <p>fucking hell i hate php !</p> -->
+  <body>
+    <?php echo '<p>Hello World</p>'; ?>
+    <a href="/index.php">Visit the home pageee.</a>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+
+
+
 </html>
