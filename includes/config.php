55b83961971d5e41dfeb65997b1a6fd48e859401
Evan <ciriel@live.com>
2019-10-17 14:38:34 -0400

    copying my assignment 2

---
+++ includes/config.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Used to store website configuration information.
+ *
+ * @var string or null
+ */
+function config($key = '')
+{
+    $config = [
+        'name' => 'Evan: Assignment 2',
+        'XKCDURL' => 'http://xkcd.com/info.0.json',
+        'pretty_uri' => false,
+        'site_url' => 'https://jonesevanassignment2.herokuapp.com',
+        'nav_menu' => [
+            '' => 'Home',
+            'about-me' => 'About Me',
+            'cross-training' => 'Cross Training'
+        ],
+        'nav_bar' => [
+          '' => 'Home',
+          'about-me'      => "About Me",
+          'random-comic' => 'Random Comic',
+          'todays-comic' => 'Todays Comic'
+        ],
+        'template_path' => 'template',
+        'content_path' => 'content',
+        'version' => 'v3.0',
+    ];
+    return isset($config[$key]) ? $config[$key] : null;
+}
