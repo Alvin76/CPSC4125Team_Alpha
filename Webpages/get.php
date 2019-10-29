3e550bbdb4079522c088fe0d54e4bc3aa5499406
James Ericsson <JEricsson@omegafi.net>
2019-10-29 10:59:13 -0400

    generic commit

--- Webpages/get.php
+++ Webpages/get.php
@@ -3,8 +3,11 @@
   <?php require '../Backend/functions.php';?>
   <?php noCache(); ?>
   <?php getMovies("test"); ?>
+  <p> "wut"</p>
   <?php getMovies(search); ?>
+  <p> "wut"</p>
   <?php getMovies("search"); ?>
+  <p> "wut"</p>
   <?php getMovies($search); ?>
   <p> "wut"</p>
 </html>
