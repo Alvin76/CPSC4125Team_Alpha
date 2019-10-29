3dbee8958490aac21761cc551f566cfc180db579
ericsson-james <43553673+ericsson-james@users.noreply.github.com>
2019-10-29 19:20:50 -0400

    Update get.php

--- Webpages/get.php
+++ Webpages/get.php
@@ -1,13 +1,12 @@
-<!doctype html>
-<html lang="en">
-  <?php require '../Backend/functions.php';?>
-  <?php noCache(); ?>
+
+<?php
+
+
+  require '../Backend/functions.php';
   
-  <?php
    if(isset($_POST['search'])) {
     echo "success"
   } else {
     echo "fail";
   };
-  ?>
-</html>
+  
