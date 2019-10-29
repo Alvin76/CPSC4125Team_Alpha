a964503c2351567bd21a4f0dbbf77918bb8f6dcb
James Ericsson <JEricsson@omegafi.net>
2019-10-29 10:36:40 -0400

    got the search feature started

---
+++ Webpages/get.php
@@ -0,0 +1,6 @@
+<!doctype html>
+<html lang="en">
+  <?php require '../Backend/functions.php';?>
+  <?php noCache(); ?>
+  <?php getMovies("test search"); ?>
+</html>
--- Webpages/omdb.php
+++ Webpages/omdb.php
@@ -1,4 +1,5 @@
 <!doctype html>
+
 <html lang="en">
   <?php require '../Backend/functions.php';?>
   <!-- Optional JavaScript -->
@@ -6,18 +7,35 @@
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <?php head(); ?>
   <body>
     <?php nav_bar(); ?>
-
     <!-- Page specific content will go down here. -->
     <div class="jumbotron jumbotron-fluid">
       <div class="container">
         <h1 class="display-4">This is team Alphas super awesome OMDB API Interfacer.</h1>
         <p class="lead">Go to the Navbar to look up some movies.</p>
-        <div class="Comic Image">
-          Comic Here
-        </div>
+
+          <input id="search" type="text" placeholder="Type Movie Here">
+          <button id="click_button">Search</button>
+          <div id="ComicImage">
+            Comic
+          </div>
+          <script>
+              $(document).ready(function(){
+              $("#click_button").click(function(){
+                $.ajax({url: "get.php", data: {
+                      search: "Test Search"
+                  }, success: function(result){
+                  console.log("Success");
+                  $("#ComicImage").html(result);
+                }});
+               });
+              });
+            </script>
+          <!-- ?php getIronMan(); ?> -->
+
       </div>
     </div>
     <!-- Page specific content ends here. -->
