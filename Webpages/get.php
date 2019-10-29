bc886ff53a3df43e56d5650ea8a340cf4e3435c5
ericsson-james <jamesericsson95@gmail.com>
2019-10-29 17:21:42 -0400

    generic commit

--- Webpages/get.php
+++ Webpages/get.php
@@ -2,14 +2,5 @@
 <html lang="en">
   <?php require '../Backend/functions.php';?>
   <?php noCache(); ?>
-  <?php getMovies("fail"); ?>
-  <p> "wut"</p>
-  <?php getMovies(search); ?>
-  <p> "wut"</p>
-  <?php getMovies("fail"); ?>
-  <p> "wut"</p>
-  <?php getMovies($search); ?>
-  <p> "wut2"</p>
-  <?php $search2 = $_POST['search']; ?>
-  <?php getMovies($search2); ?>
+  <?php getIronMan() ?>
 </html>
