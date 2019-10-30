55d98c3618901e8ca27f735edeeaa80b51ee8a4d
ericsson-james <jamesericsson95@gmail.com>
2019-10-29 22:56:39 -0400

    generic commit

--- Webpages/get.php
+++ Webpages/get.php
@@ -10,6 +10,9 @@
 require '../Backend/functions.php';
 
 if(isset($_POST['search'])) {
+  echo $_POST['search'];
+  echo "pass/fail?"
+
   $testingGoal = $_POST['search'];
  echo testgetMovies($testingGoal);
 } else {
