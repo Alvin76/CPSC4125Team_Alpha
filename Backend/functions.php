fe0efdf9cb529490a96f591ebe2c577f90fbe0c7
ericsson-james <jamesericsson95@gmail.com>
2019-10-29 22:18:22 -0400

    generic commit

--- Backend/functions.php
+++ Backend/functions.php
@@ -34,7 +34,7 @@ function head(){
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <!-- Required PHP Functions -->
 
-    <title>James Ericsson</title>
+    <title>Team Alpha</title>
   </head>';
   echo $head;
 }
@@ -166,9 +166,5 @@ function getIronMan(){
 
 function testgetMovies($search){
     console.log("WOW");
-}
-
-function getMovies($search){
-  echo "$search";
 }
 ?>
--- Webpages/get.php
+++ Webpages/get.php
@@ -9,6 +9,7 @@
 <?php
 if(isset($_POST['search'])) {
  echo $_POST['search'];
+ testgetMovies($_POST['search']);
 } else {
  echo "fail";
 };
