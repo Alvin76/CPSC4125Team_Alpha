ccd22d59befd863942b1f2c1c285949c0040ca33
woods-taylor <43648944+Woods-Taylor@users.noreply.github.com>
2019-10-29 19:57:38 -0500

    adding contact us page

--- functions.php
+++ functions.php
@@ -6,8 +6,8 @@ $color='red';
 $car='BMW';
 $websiteURL = 'https://test-app-ericsson.herokuapp.com/';
 $authors = array('James_Ericsson');
-$nav_items = array('Home Page','About Us');
-$urls = array('Home.php', 'aboutus.php');
+$nav_items = array('Home Page','About Us', 'Contact Us');
+$urls = array('Home.php', 'aboutus.php', 'contactus.php');
 
 function noCache(){
   header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
