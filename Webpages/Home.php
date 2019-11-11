8a486a31e909cd35782818c0af2927ed1c422f8b
Evan <ciriel@live.com>
2019-11-11 16:51:36 -0500

    Adding my carousel. Needs a few adjustments. Will leave Jumbotron for now until I consult with you all

--- Webpages/Home.php
+++ Webpages/Home.php
@@ -17,6 +17,141 @@
 
 
     <!-- Page specific content will go down here. -->
+
+    <!--Carousel Wrapper-->
+ <div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
+
+   <!--Indicators-->
+   <ol class="carousel-indicators">
+     <li data-target="#carousel-example-1z" data-slide-to="0" class="active"></li>
+     <li data-target="#carousel-example-1z" data-slide-to="1"></li>
+     <li data-target="#carousel-example-1z" data-slide-to="2"></li>
+   </ol>
+   <!--/.Indicators-->
+
+   <!--Slides-->
+   <div class="carousel-inner" role="listbox">
+
+     <!--First slide-->
+     <div class="carousel-item active">
+       <div class="view" style="background-image: url('https://images5.alphacoders.com/887/887355.jpg'); background-repeat: no-repeat; background-size:contain;">
+
+         <!-- Mask & flexbox options-->
+         <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+
+           <!-- Content -->
+           <div class="text-center white-text mx-5 wow fadeIn">
+             <h1 class="mb-4">
+               <strong>TAMD</strong>
+             </h1>
+
+             <p>
+               <strong>Team Alpha Movie Database</strong>
+             </p>
+
+             <p class="mb-4 d-none d-md-block">
+               <strong>Online Movie Database service used for obtaining information regarding all of your favorite films</strong>
+             </p>
+
+             <a target="_blank" href="https://team-alpha-web-site.herokuapp.com/Webpages/omdb.php" class="btn btn-outline-white btn-lg">Get Started
+               <i class="fa fa-graduation-cap ml-2"></i>
+             </a>
+           </div>
+           <!-- Content -->
+
+         </div>
+         <!-- Mask & flexbox options-->
+
+       </div>
+     </div>
+     <!--/First slide-->
+
+     <!--Second slide-->
+     <div class="carousel-item">
+       <div class="view" style="background-image: url('https://images5.alphacoders.com/757/757038.jpg'); background-repeat: no-repeat; background-size:contain;">
+
+         <!-- Mask & flexbox options-->
+         <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+
+           <!-- Content -->
+           <div class="text-center white-text mx-5 wow fadeIn">
+             <h1 class="mb-4">
+               <strong>TAMD</strong>
+             </h1>
+
+             <p>
+               <strong>Team Alpha Movie Database</strong>
+             </p>
+
+             <p class="mb-4 d-none d-md-block">
+               <strong>Online Movie Database service used for obtaining information regarding all of your favorite films</strong>
+             </p>
+
+             <a target="_blank" href="https://team-alpha-web-site.herokuapp.com/Webpages/omdb.php" class="btn btn-outline-white btn-lg">Get Started
+               <i class="fa fa-graduation-cap ml-2"></i>
+             </a>
+           </div>
+           <!-- Content -->
+
+         </div>
+         <!-- Mask & flexbox options-->
+
+       </div>
+     </div>
+     <!--/Second slide-->
+
+     <!--Third slide-->
+     <div class="carousel-item">
+       <div class="view" style="background-image: url('https://images4.alphacoders.com/762/76276.jpg'); background-repeat: no-repeat; background-size:contain;">
+
+         <!-- Mask & flexbox options-->
+         <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+
+           <!-- Content -->
+           <div class="text-center white-text mx-5 wow fadeIn">
+             <h1 class="mb-4">
+               <strong>TAMD</strong>
+             </h1>
+
+             <p>
+               <strong>Team Alpha Movie Database</strong>
+             </p>
+
+             <p class="mb-4 d-none d-md-block">
+               <strong>Online Movie Database service used for obtaining information regarding all of your favorite films</strong>
+             </p>
+
+             <a target="_blank" href="https://team-alpha-web-site.herokuapp.com/Webpages/omdb.php" class="btn btn-outline-white btn-lg">Get Started
+               <i class="fa fa-graduation-cap ml-2"></i>
+             </a>
+           </div>
+           <!-- Content -->
+
+         </div>
+         <!-- Mask & flexbox options-->
+
+       </div>
+     </div>
+     <!--/Third slide-->
+
+   </div>
+   <!--/.Slides-->
+
+   <!--Controls-->
+   <a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
+     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+     <span class="sr-only">Previous</span>
+   </a>
+   <a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
+     <span class="carousel-control-next-icon" aria-hidden="true"></span>
+     <span class="sr-only">Next</span>
+   </a>
+   <!--/.Controls-->
+
+ </div>
+ <!--/.Carousel Wrapper-->
+
+
     <div class="jumbotron jumbotron-fluid">
       <div class="container">
         <h1 class="display-4">This is team Alphas super awesome OMDB API Interfacer.</h1>
