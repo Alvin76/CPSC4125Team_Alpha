83f718e0bd51b775c3a526de83bb72141b1143fb
Evan <ciriel@live.com>
2019-11-17 15:56:29 -0500

    retry before revert

--- Backend/functions.php
+++ Backend/functions.php
@@ -185,15 +185,15 @@ function testgetMovies($search){
   $NumberOfElements = count($response['Search']);
 //  echo $NumberOfElements;
   for ($x = 0; $x <= $NumberOfElements ; $x++) {
-    echo '<div class="container">
+    echo "<div class='container'>
   <div class="row">
-    <div class="col-md-4">.col-md-4</div>
-    <div class="col-md-4">.col-md-4</div>
-    <div class="col-md-4">.col-md-4</div>
+    <div class="col-md-4">.$response['Search'][$x]["Title"].'<br>';</div>
+    <div class="col-md-4">  $imageURL = $response['Search'][$x]['Poster'];</div>
   </div>
-</div>'.$response['Search'][$x]["Title"].'<br>';
-    $imageURL = $response['Search'][$x]['Poster'];
-    echo '<img src=" '.$imageURL.' " alt="ironmen">';
+</div>"
+// .$response['Search'][$x]["Title"].'<br>';
+//     $imageURL = $response['Search'][$x]['Poster'];
+//     echo '<img src=" '.$imageURL.' " alt="ironmen">';
   //  echo '<img src=" '.$response['Search'][$x]['Poster'].' " alt="ironman">';
   //  echo $response['Search'][$x]['Poster']
   }
