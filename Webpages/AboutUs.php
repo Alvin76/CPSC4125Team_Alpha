e1405190b78b34e7caf93eedf25062094936fc25
ericsson-james <jamesericsson95@gmail.com>
2019-11-18 22:34:41 -0500

    generic commit

--- Webpages/AboutUs.php
+++ Webpages/AboutUs.php
@@ -61,6 +61,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="../CSS/style.css" rel="stylesheet">
     <?php require '../Backend/functions.php';?>
     <title>About Us</title>
   </head>
