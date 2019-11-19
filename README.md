2e00acc3cf5b884aa4d5960ccb786eef8d34981e
ericsson-james <jamesericsson95@gmail.com>
2019-11-18 21:57:25 -0500

    testing sprint8

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 This website is for us to learn how to write in PHP and be able to work in team using Freedcamp and Github
 This website has 4 pages, home page, results page, About-us page, and contact-us
-This website has OMDB API interface to look up movies 
+This website has OMDB API interface to look up movies
 My Heroku Link: https://team-alpha-web-site.herokuapp.com//?page=Menu
 
 Activating Ericsson
@@ -8,3 +8,4 @@ Activating Ericsson
 Ericsson Activated
 Coding ON
 lol
+Testing a push to branch Sprint8.
--- Webpages/AboutUs.php
+++ Webpages/AboutUs.php
@@ -8,7 +8,7 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   <?php head(); ?>
-  
+
   <style>
   <?php include '../CSS/style.css'; ?>
   </style>
@@ -21,7 +21,7 @@
       <div class="container">
         <main>
           <div class="text-center">
-            <article>Welcome to our OMDB search website. </article>
+            <article>Welcome to our OMDB search website. TEXT</article>
             <img src='../Images/alpaca-stare.jpg' alt='photo of me' />
           </div>
           <div class="container">
