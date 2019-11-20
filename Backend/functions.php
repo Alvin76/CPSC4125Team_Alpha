ae1e89e818722e70b1bc48de398ecd0ea70f27ac
woods-taylor <43648944+Woods-Taylor@users.noreply.github.com>
2019-11-19 20:46:56 -0500

    testing navbar

--- Backend/functions.php
+++ Backend/functions.php
@@ -40,7 +40,7 @@ function nav_bar()
         Alpha Alpacas
       </a>
     </nav>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar navbar-expand-lg" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
