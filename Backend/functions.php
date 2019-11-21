4414beb83174e2c10ad88c3b41b3250ca7d2ff26
woods-taylor <43648944+Woods-Taylor@users.noreply.github.com>
2019-11-21 15:26:31 -0500

    expermimenting with changes to search in preperation for adding modal

--- Backend/functions.php
+++ Backend/functions.php
@@ -107,6 +107,7 @@ function getMovies($search){
      echo '<div class="box'.$x.'">';
      echo $response["Search"][$x]["Title"].'<br>';
      echo '<img src=" '.$response['Search'][$x]['Poster'].' " alt="No-Image-Available"><br>';
+     echo '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Launch demo modal</button>';
      echo $modal1;
      echo $modal2;
      /*details($response["Search"][$x]["imdbID"]);*/
