825624b82fbca327bbb1de55dae46aba5ec74504
woods-taylor <43648944+Woods-Taylor@users.noreply.github.com>
2019-11-20 20:42:04 -0500

    expermimenting with changes to search in preperation for adding modal

--- Backend/functions.php
+++ Backend/functions.php
@@ -64,7 +64,6 @@ function footer()
 
 function getMovies($search){
 
-
   $url = 'http://omdbapi.com/?apikey=ddbdfa64&s=%27'.urlencode($search);
   echo "<p style=\"color:#0E0816\">".$url."</p>";
   $handle = curl_init();
@@ -85,37 +84,7 @@ function getMovies($search){
      echo '<div class="box'.$x.'">';
      echo $response["Search"][$x]["Title"].'<br>';
      echo '<img src=" '.$response['Search'][$x]['Poster'].' " alt="No-Image-Available"><br>';
-     echo  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Info
-     </button>;
-     /*details($response["Search"][$x]["imdbID"]);*/
-
-
-     /*$modal1 ='<!-- Modal -->
- <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">';
-
-    $modal2 =  '</div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>';*/
-
-/*echo $modal1
-echo details($response["Search"][$x]["imdbID"]);
-echo $modal2*/
-
-
+     details($response["Search"][$x]["imdbID"]);
     echo '</div>';
   }
   echo '</div>';
