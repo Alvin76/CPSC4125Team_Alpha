153ef52647e2362edef7a09b38e005ef7ce0450f
ericsson-james <jamesericsson95@gmail.com>
2019-11-22 23:32:39 -0500

    generic commit

--- Backend/functions.php
+++ Backend/functions.php
@@ -80,25 +80,18 @@ function getMovies($search){
   $NumberOfElements = count($response['Search']);
   $imageURL = $response['Search'][$x]['Poster'];
 
-  $modal1 ='<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">';
-
-  $modal2 = '</div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>';
+  // $modal1 ='<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  // <div class="modal-dialog" role="document">
+  //   <div class="modal-content">
+  //     <div class="modal-header">
+  //       <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+  //       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+  //         <span aria-hidden="true">&times;</span>
+  //       </button>
+  //     </div>
+  //     <div class="modal-body">';
+//
+  // $modal2 = '</div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button><button type="button" class="btn btn-primary">Save changes</button></div></div></div></div>';
 
 
 
@@ -108,9 +101,18 @@ function getMovies($search){
      echo $response["Search"][$x]["Title"].'<br>';
      echo '<img src=" '.$response['Search'][$x]['Poster'].' " alt="No-Image-Available"><br>';
      echo '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">View Plot</button>';
-     echo $modal1;
+     echo '<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">'
+     echo '<div class="modal-dialog" role="document">'
+     echo '<div class="modal-content">'
+    echo     '<div class="modal-header">'
+      echo     '<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>'
+      echo     '<button type="button" class="close" data-dismiss="modal" aria-label="Close">'
+      echo     '<span aria-hidden="true">&times;</span>'
+        echo   '</button>'
+      echo   '</div>'
+      echo '   <div class="modal-body">';
      details($response["Search"][$x]["imdbID"]);
-     echo $modal2;
+     echo '</div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button><button type="button" class="btn btn-primary">Save changes</button></div></div></div></div>'
      /*details($response["Search"][$x]["imdbID"]);*/
     echo '</div>';
   }
