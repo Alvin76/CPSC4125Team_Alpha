ba8802c1606bb902cafb1c7acaff7f8916599361
ericsson-james <jamesericsson95@gmail.com>
2019-11-23 00:03:36 -0500

    generic commit

--- Backend/functions.php
+++ Backend/functions.php
@@ -121,7 +121,7 @@ function getMovies($search){
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLongTitle">'.$response["Search"][$x]["Title"].'</h5>
+            <h5 class="modal-title" id="exampleModalLongTitle'.$x.'">'.$response["Search"][$x]["Title"].'</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
